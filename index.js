c28e1e84825b8ad5fc6d6482735ea11d08f4d294
Victoria Meng <nights1ststar@protonmail.com>
2017-11-08 01:31:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver() {
+  drivers.push("Ralph");
+}
+
+function destructivelyPrependDriver() {
+  drivers.unshift("Bob");
+}
+
+function destructivelyRemoveLastDriver() {
+  drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver() {
+  drivers.shift();
+}
+
+function appendDriver() {
+  return drivers.concat(["Broom"]);
+}
+
+function prependDriver() {
+  return ["Arnold"].concat(drivers);
+}
+
+function removeLastDriver() {
+  const newDrivers = drivers.slice(-1);
+}
